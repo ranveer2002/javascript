b3bb18297407972e9c9f9e1af2b637a5d136898b
Ranveer Singh <114814358+ranveer2002@users.noreply.github.com>
2024-09-10 21:39:44 +0530

    datatypes in  js

---
+++ basics/datatypes.js
@@ -0,0 +1,36 @@
+// primitive datatype
+ 
+// number , string , bool 
+
+let x = 4 ;
+
+let y = "hello" ;
+
+let z = true;
+
+console.log(typeof x);
+
+console.log(typeof y);
+
+console.log(typeof z);
+
+// non-primitive datatype
+
+//array function object
+
+const arr = [1,2,34,5,6,7];
+
+console.log(arr);
+
+let obj = {
+    name : "john",
+    value: 100
+}
+
+console.log(obj);
+
+function sum (a,b){
+    return a+b;
+}
+sum(4,5);
+
