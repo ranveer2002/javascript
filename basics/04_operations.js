a3811fd2577e22d6c58b3e3209ab0128f560632b
Ranveer Singh <114814358+ranveer2002@users.noreply.github.com>
2024-09-11 21:56:32 +0530

    arithmatic operations and comparison in js

---
+++ basics/04_operations.js
@@ -0,0 +1,36 @@
+let value = 3;
+
+// console.log(value);
+
+let negValue = -value
+
+// console.log(negValue);
+
+// console.log(2+2)
+// console.log(2-2)
+// console.log(2*2)
+// console.log(2**2) // 2 ^ 2
+// console.log(2**3) // 2 ^ 3
+// console.log(2/2)
+// console.log(2%2)
+
+
+let str1 = "Ranveer";
+let str2 = "Singh";
+
+// console.log(str1 + " " + str2)
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log(1 + 3 + "2");
+// console.log("1" +2 + "2");
+
+// console.log(+true) // 1
+// console.log(+"") // 0
+
+let first = 100;
+
+// console.log(first++);
+// console.log(++first);
+// console.log(first--);
+// console.log(--first);
