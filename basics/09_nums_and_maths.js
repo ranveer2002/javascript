2d00b7ad9e3001f5fa8530738a8def8b8e922801
Ranveer Singh <114814358+ranveer2002@users.noreply.github.com>
2024-10-17 21:40:07 +0530

    maths and nums in js

---
+++ basics/09_nums_and_maths.js
@@ -0,0 +1,69 @@
+
+const score = 90;
+
+// console.log(score);
+
+// console.log(typeof score)
+
+
+const balance = new Number(700);
+
+// console.log(balance)
+
+// console.log(typeof balance);
+
+// console.log(typeof balance.toString());
+
+// console.log( balance.toFixed(2));
+
+
+const otherNumber = 24.84673;
+
+// console.log(otherNumber.toPrecision(4));
+
+const newNumber = 1000000000;
+
+// console.log(newNumber.toLocaleString());
+
+
+// console.log(newNumber.toExponential(5));
+
+// console.log(Number.MAX_VALUE)
+
+// console.log(Number.MIN_VALUE)
+
+// console.log(Number.MAX_SAFE_INTEGER)
+
+// console.log(Number.MIN_SAFE_INTEGER)
+
+
+//++++++++++++++++++++++++++++++ Maths ++++++++++++++++++++++++++++++++//
+
+
+
+// console.log(Math)
+
+// console.log(Math.E)
+
+// console.log(Math.ceil(4.598))
+
+// console.log(Math.floor(4.598))
+
+// console.log(Math.abs(4))
+
+// console.log(Math.abs(-4))
+
+// console.log(Math.round(2.8948));
+
+// console.log(Math.round(2.5100));
+
+// console.log(Math.max(2,4,1,6,7,0,3))
+
+// console.log(Math.min(2,4,1,6,7,0,3))
+
+console.log(Math.floor(Math.random()*6 +1))
+
+const max = 10; 
+const min = 20;
+
+console.log(Math.floor(Math.random()*(max-min+1))+min)
